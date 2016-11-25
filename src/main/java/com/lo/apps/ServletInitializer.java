a941a1ee69661f9d2eab5285c1730ab27cad6e5d
Miloš Poljak <milos.poljak@prozone.rs>
2016-11-25 14:26:22 +0100

    init commit

---
+++ src/main/java/com/lo/apps/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lo.apps;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GalleryApplication.class);
+	}
+
+}
---
+++ src/main/webapp/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('webapp', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
